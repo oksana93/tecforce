874714270edebeaf13e5d00da27877807a23006d
antoshinaov <antoshinaov@altarix.ru>
2018-12-19 13:24:27 +0400

    Merge branch 'master' of E:\Oksana\IntellijIdea\test with conflicts.

---
+++ src/test/resources/db.migration/V1__create_initial_schema.sql
@@ -0,0 +1,23 @@
+create table city (
+  id    uuid primary key,
+  name  varchar not null,
+  woeid int     not null,
+  code  int     not null
+);
+
+create table forecast (
+  id           uuid primary key,
+  image_url    varchar,
+  date         date not null default current_date,
+  day          varchar not null default to_char(current_date, 'dy'),
+  max_wind     int,
+  min_wind     int,
+  current_wind int,
+  max_temp     int,
+  min_temp     int,
+  current_temp int,
+  city_id      uuid not null,
+  text         varchar,
+  foreign key (city_id) references city (id)
+);
+
