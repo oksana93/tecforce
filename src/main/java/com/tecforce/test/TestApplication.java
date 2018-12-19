543d352c22378eb245d7c485f1ee871a6fae1735
antoshinaov <antoshinaov@altarix.ru>
2018-12-19 13:49:33 +0400

    Readme

--- src/main/java/com/tecforce/test/TestApplication.java
+++ src/main/java/com/tecforce/test/TestApplication.java
@@ -1,10 +1,8 @@
 package com.tecforce.test;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootApplication
 @EnableScheduling
